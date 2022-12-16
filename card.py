99173faf99ec0cfaa3b0dd30549b25b4d1024e34
Maksim <playmakcim228@gmail.com>
2022-12-16 19:21:04 +0500

    переключение карты

--- card.py
+++ card.py
@@ -3,16 +3,18 @@ import sqlite3
 
 
 class Card(pg.sprite.Sprite):  # класс Карты
-    def __init__(self, size, card_id, land=-1):  # получает размер и id карты
+    def __init__(self, size: tuple, card_id: int, land=-1):  # получает размер и id карты
         pg.sprite.Sprite.__init__(self)
         self.id = card_id
         self.land = land
         self.image = pg.transform.scale(pg.image.load(f'cards/{card_id}.png').convert_alpha(), size)
         self.pos = (0, 0)
         self.rect = pg.Rect(0, 0, *size)
+        self.default: pg.Rect = self.rect
         con = sqlite3.connect('card_war.db')
         cur = con.cursor()
         info = cur.execute(f'SELECT * FROM cards WHERE id = {card_id}').fetchall()[0]
+        self.case = 0
 
         self.status = 0  # свойство карты
         self.price = info[2]
@@ -21,37 +23,63 @@ class Card(pg.sprite.Sprite):  # класс Карты
         if not self.object == 2:
             self.floop = info[5]
             self.floop_price = info[6]
-            if not self.object:
+            if self.object == 0:
                 self.atc = info[7]
                 self.hp = info[8]
+                self.specifications()
 
     def zeroing(self) -> None:  # возращает карту на место (pos)
         self.rect.move_ip(self.pos[0] - self.rect.x, self.pos[1] - self.rect.y)
+        self.default.move_ip(self.pos[0] - self.default.x, self.pos[1] - self.default.y)
 
-    def dead(self, cemetery) -> None:  # уберает карту в сброс
+    def dead(self, cemetery: tuple) -> None:  # уберает карту в сброс
         self.rect.move_ip(cemetery[0] - self.pos[0], cemetery[1] - self.pos[1])
         self.status = -1
         self.land = -1
 
-    def set_land(self, land) -> None:
+    def set_land(self, land: pg.Rect) -> None:
         self.rect = land
+        self.default = land
         self.pos = (self.rect.x, self.rect.y)
         self.status = 2  # устанавливаем новый статус карты
 
-    def location(self, n, hand_pos, indent, slider) -> None:
+    def location(self, n: int, hand_pos: tuple, indent: tuple, slider: int) -> None:
         self.pos = (
             hand_pos[0] + indent[0] * (n & 1), hand_pos[1] + indent[1] * (n >> 1) - slider * (self.rect.h >> 2))
         self.zeroing()
 
-    def drawing(self, surface: pg.Surface, hand_pos) -> None:
+    def drawing(self, surface: pg.Surface, hand_pos: tuple) -> None:
         surface.blit(self.image,
                      pg.Rect(self.rect.x - hand_pos[0], self.rect.y - hand_pos[1], self.rect.w, self.rect.h))
 
-    def scroll(self, n) -> None:
+    def scroll(self, n: int) -> None:
         self.pos = (self.pos[0], self.pos[1] + (self.rect.h >> 2) * n)
         self.zeroing()
 
-    def update(self, pos: tuple, surface: pg.Surface, size: tuple) -> None:
-        if self.rect.collidepoint(pg.mouse.get_pos()):
-            surface.blit(pg.transform.scale(pg.image.load(f'cards/{self.id}.png'), size),
-                         pg.Rect(*pos, *size))
+    def viev(self, new: pg.Rect):
+        self.rect = new.copy()
+        self.pos = (new.x, new.y)
+        self.image = pg.transform.rotate(self.image, 180)
+
+    def alt(self, img, size):
+        res = pg.transform.scale(img, size)
+        if self.object == 0:
+            pg.draw.rect(res, (255, 255, 255),
+                         pg.Rect(size[0] * 0.819, size[1] * 0.8625, size[0] * 0.1, size[1] * 0.0721))
+            pg.draw.rect(res, (255, 255, 255),
+                         pg.Rect(size[0] * 0.101, size[1] * 0.863, size[0] * 0.08, size[1] * 0.0725))
+            font = pg.font.Font('base.ttf', 48)
+            res.blit(font.render(str(self.hp), True, (0, 0, 0)), (size[0] * 0.819, size[1] * 0.88))
+            res.blit(font.render(str(self.hp), True, (0, 0, 0)), (size[0] * 0.101, size[1] * 0.88))
+        return res
+
+    def specifications(self):
+        pg.draw.rect(self.image, (255, 255, 255),
+                     pg.Rect(self.default.w * 0.825, self.default.h * 0.87,
+                             self.default.w * 0.095, self.default.h * 0.075))
+        pg.draw.rect(self.image, (255, 255, 255),
+                     pg.Rect(self.default.w * 0.1, self.default.h * 0.87,
+                             self.default.w * 0.095, self.default.h * 0.075))
+        font = pg.font.Font('base.ttf', 12)
+        self.image.blit(font.render(str(self.hp), True, (0, 0, 0)), (self.default.w * 0.825, self.default.h * 0.88))
+        self.image.blit(font.render(str(self.hp), True, (0, 0, 0)), (self.default.w * 0.11, self.default.h * 0.88))
--- logic_of_game.py
+++ logic_of_game.py
@@ -14,8 +14,11 @@ def pvp(screen: pg.Surface, W: int, H: int, decks: list) -> None:
     sard_w = int(sard_h * 0.72)
     cemetery = ((W >> 1) - int(card_w * 3.7), (H >> 1))
     deck = ((W >> 1) - int(card_w * 2.85), (H >> 1))
-    size_rect_x = card_w // 20 * 3 + (W >> 1)
-    size_rect_y = card_h // 7 + (H >> 1)
+    rect_attack = pg.Rect((W >> 1) - (card_w << 1), (H >> 1) - sard_w, card_w << 2, sard_w)
+    rect_floop = pg.Rect((W >> 1) - card_w * 2, (H >> 1) + card_h, card_w * 4, sard_w)
+    btn_end = pg.Rect((sard_h >> 2, H - (sard_w >> 1)), (card_h, sard_h >> 2))
+    size_rect_x = sard_w // 20 * 4.75 + (W >> 1)
+    size_rect_y = sard_h // 7
     PLAYER_1 = Player(decks.pop(randint(-1, 1)))
     [PLAYER_1.land.add(Land(PLAYER_1.land_id[i], ((W >> 1) - card_w * (2 - i), (H >> 1)), (card_w, card_h), 0)) for i in
      range(4)]
@@ -24,15 +27,20 @@ def pvp(screen: pg.Surface, W: int, H: int, decks: list) -> None:
         Land(PLAYER_2.land_id[i], ((W >> 1) - card_w * (2 - i), (H >> 1) - card_h), (card_w, card_h), 180)) for i in
         range(4)]
 
-    hp_pos = ((W >> 1) - int(card_w * 3.3), H - sard_w)
-    action_pos = ((W >> 1) - int(card_w * 2.7), H - sard_w)
+    hp_pos = ((W >> 1) - int(card_w * 3.15), H - sard_w)
+    action_pos = ((W >> 1) - int(card_w * 2.55), H - sard_w)
 
     bg = pg.transform.scale(pg.image.load("bg.png"), size)  # фон и атрибуты игры
     deck_card = pg.transform.scale(pg.image.load("cards/back.png"), (sard_w, sard_h))
     btn_deck = pg.Rect(*deck, sard_w, sard_h)
     # Расположение существ на полях
-    rect_card = ([pg.Rect(size_rect_x + card_w * (1 - i), size_rect_y, sard_w, sard_h) for i in range(4)],
-                 [pg.Rect(size_rect_x + card_w * (1 - i), size_rect_y + sard_h, sard_w, sard_h) for i in range(4)])
+    rect_card = ([pg.Rect(size_rect_x + card_w * (1 - i), size_rect_y + (H >> 1), sard_w, sard_h) for i in range(4)],
+                 [pg.Rect(size_rect_x + card_w * (1 - i), size_rect_y + sard_h + (H >> 1), sard_w, sard_h) for i in
+                  range(4)])
+    rect_card_2 = (
+        [pg.Rect(size_rect_x + card_w * (1 - i), -size_rect_y + (H >> 1) - sard_h, sard_w, sard_h) for i in range(4)],
+        [pg.Rect(size_rect_x + card_w * (1 - i), -size_rect_y - sard_h + (H >> 1) - sard_h, sard_w, sard_h) for i in
+         range(4)])
     hand = pg.Surface((int(card_w * 1.6), int(card_h * 2.1)))
     hand_rect = pg.Rect((W >> 1) + int(card_w * 2.1), (H >> 1) - sard_h, *hand.get_size())
     win = None
@@ -52,10 +60,35 @@ def pvp(screen: pg.Surface, W: int, H: int, decks: list) -> None:
             PLAYER_1.land.add(
                 Land(PLAYER_1.land_id[i], ((W >> 1) - card_w * (2 - i), (H >> 1)), (card_w, card_h), 0,
                      PLAYER_1.land_activ[i]))
+            if PLAYER_1.active_cards[0][i]: PLAYER_1.active_cards[0][i].viev(rect_card[0][i])
+            if PLAYER_1.active_cards[1][i]: PLAYER_1.active_cards[1][i].viev(rect_card[1][i])
             PLAYER_2.land.add(
                 Land(PLAYER_2.land_id[i], ((W >> 1) - card_w * (2 - i), (H >> 1) - card_h), (card_w, card_h), 180,
                      PLAYER_2.land_activ[i]))
+            if PLAYER_2.active_cards[0][i]: PLAYER_2.active_cards[0][i].viev(rect_card_2[0][i])
+            if PLAYER_2.active_cards[1][i]: PLAYER_2.active_cards[1][i].viev(rect_card_2[1][i])
         timeee = True
+
+        [a.location(n,
+                    (hand_rect.x + int(sard_w * 0.125),
+                     hand_rect.y + int(sard_w * 0.125)),
+                    (sard_w, sard_h), slider) for n, a in enumerate(cards_on_hand)]
+
+        for c in cards:
+            if c and c.case != 0:
+                c.case = 0
+                c.status = 2
+        for c in PLAYER_2.active_cards[0]:
+            if c and c.case == 1:
+                c.rect.w, c.rect.h = sard_h, sard_w
+                c.image = pg.transform.rotate(c.image, 90)
+                c.pos = (c.pos[0] - size_rect_x + (W >> 1), c.pos[1] + size_rect_y + sard_h - sard_w)
+                c.zeroing()
+            if c and c.case == 2:
+                c.rect.w, c.rect.h = sard_h, sard_w
+                c.image = pg.transform.rotate(c.image, 90)
+                c.pos = (c.pos[0] - size_rect_x + (W >> 1), c.pos[1])
+                c.zeroing()
 
         while runGame and timeee:
             screen.blit(bg, (0, 0))  # отрисововаем фон
@@ -68,10 +101,10 @@ def pvp(screen: pg.Surface, W: int, H: int, decks: list) -> None:
                 if event.type == pg.KEYDOWN:
                     if event.key == pg.K_ESCAPE:  # выход из игры
                         runGame = False
-                    if event.key == pg.K_BACKSPACE:
-                        timeee = False
                 if event.type == pg.MOUSEBUTTONUP:
                     if event.button == 1:
+                        if btn_end.collidepoint(pg.mouse.get_pos()):
+                            timeee = False
                         if cur:
                             if cur.object < 2:
                                 for i in range(4):
@@ -87,6 +120,18 @@ def pvp(screen: pg.Surface, W: int, H: int, decks: list) -> None:
                                                     (hand_rect.x + int(sard_w * 0.125),
                                                      hand_rect.y + int(sard_w * 0.125)),
                                                     (sard_w, sard_h), slider) for n, a in enumerate(cards_on_hand)]
+                            if cur.object == 0 and rect_attack.colliderect(cur.rect):
+                                cur.case = 1
+                                cur.rect.w, cur.rect.h = sard_h, sard_w
+                                cur.image = pg.transform.rotate(cur.image, 90)
+                                cur.pos = (cur.pos[0] - size_rect_x + (W >> 1), cur.pos[1] - size_rect_y)
+                                cur.status = 1
+                            if cur.object == 0 and rect_floop.colliderect(cur.rect):
+                                cur.case = 2
+                                cur.rect.w, cur.rect.h = sard_h, sard_w
+                                cur.image = pg.transform.rotate(cur.image, 90)
+                                cur.pos = (cur.pos[0] - size_rect_x + (W >> 1), cur.pos[1] + sard_w * 0.4)
+                                cur.status = 1
 
                             cur.zeroing()  # возращаем карту на место
                             cur = None
@@ -114,7 +159,7 @@ def pvp(screen: pg.Surface, W: int, H: int, decks: list) -> None:
                                 cur = lis[0] if (lis := [c for c in a if c.rect.collidepoint(event.pos) and (
                                         hand_rect.collidepoint(pg.mouse.get_pos()) or c.status >= 2)]) != [] else None
                             if cur:
-                                if cur.status >= 0:
+                                if cur.status == 0 or cur.status == 2:
                                     # перемещение карты
                                     cur.rect.move_ip(event.rel)
                                 else:
@@ -128,19 +173,29 @@ def pvp(screen: pg.Surface, W: int, H: int, decks: list) -> None:
                               ((i.x - sard_w * 0.05, i.y - sard_w * 0.05), (sard_w * 1.1, sard_h + sard_w * 0.1)),
                               int(sard_w * 0.05)) for i in rect_card[cur.object]]
 
-            # setting = []
-            # for g in setting:
             pg.draw.circle(screen, (200, 0, 0), hp_pos, sard_h >> 2)
-            pg.draw.circle(screen, (100, 0, 0), ((W >> 1) - int(card_w * 3.3), H - sard_w), sard_h >> 2, 10)
+            pg.draw.circle(screen, (100, 0, 0), hp_pos, sard_h >> 2, 10)
             pg.draw.circle(screen, (0, 200, 0), action_pos, sard_h >> 2)
-            pg.draw.circle(screen, (0, 100, 0), ((W >> 1) - int(card_w * 2.7), H - sard_w), sard_h >> 2, 10)
+            pg.draw.circle(screen, (0, 100, 0), action_pos, sard_h >> 2, 10)
+            pg.draw.rect(screen, (100, 0, 0), btn_end)
 
             cards.draw(screen)  # отрисововаем карты
+            PLAYER_2.cards.draw(screen)
             [i.drawing(hand, (hand_rect.x, hand_rect.y)) for i in cards_on_hand]
             screen.blit(hand, hand_rect)
             if cur:
                 screen.blit(cur.image, cur.rect)
             if pg.key.get_pressed()[pg.K_LALT]:
-                cards.update(((W >> 1) - card_w * 1.5, 0), screen, (card_w * 3, H))
-                cards_on_hand.update(((W >> 1) - card_w * 1.5, 0), screen, (card_w * 3, H))
+                [screen.blit(card.alt(pg.image.load(f'cards/{card.id}.png'), (card_w * 3, H)),
+                             pg.Rect((W >> 1) - card_w * 1.5, 0, card_w * 3, H)) for card in cards
+                 if card.rect.collidepoint(pg.mouse.get_pos())]
+                [screen.blit(card.alt(pg.image.load(f'cards/{card.id}.png'), (card_w * 3, H)),
+                             pg.Rect((W >> 1) - card_w * 1.5, 0, card_w * 3, H)) for card in cards_on_hand
+                 if card.rect.collidepoint(pg.mouse.get_pos())]
+                [screen.blit(card.alt(pg.image.load(f'cards/{card.id}.png'), (card_w * 3, H)),
+                             pg.Rect((W >> 1) - card_w * 1.5, 0, card_w * 3, H)) for card in PLAYER_2.cards
+                 if card.rect.collidepoint(pg.mouse.get_pos())]
             pg.display.update()
+        for c in sum(map(list, (PLAYER_2.active_cards[0], cards)), []):
+            if c and c.case != 0:
+                c.image = pg.transform.rotate(c.image, -90)
